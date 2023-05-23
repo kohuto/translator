a14ca32ea15f7798aac264da7a140bb5c3d9cd6d
User Name <email@example.com>
2023-05-23 23:52:31 +0200

    updated

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import React from 'react';
-const root = createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(<App />
-);
