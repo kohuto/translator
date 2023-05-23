e44bc001236afd40989cafabd0d08692c5856fda
User Name <email@example.com>
2023-05-24 00:45:46 +0200

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import './index.css';
-
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
